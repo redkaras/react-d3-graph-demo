0e014b8aa87fc3b4947744e2c224a3c91d402e84
WarriorLin <197324984@qq.com>
2017-02-07 15:37:06 +0800

    skeleton

--- src/test-setup.js
+++ src/test-setup.js
@@ -1,9 +1,8 @@
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
+import { jsdom } from 'jsdom';
 
 chai.use(chaiImmutable);
-
-const jsdom = require('jsdom').jsdom;
 
 global.document = jsdom('<body></body>');
 global.window = document.defaultView;
